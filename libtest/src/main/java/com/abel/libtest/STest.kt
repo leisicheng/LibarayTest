b2fd7b3edfd2b6d3535fab26cbdd4133ec5de606
Abel_lei <leisicheng@baffedu.com>
2019-08-27 18:10:20 +0800

    create libtest

---
+++ libtest/src/main/java/com/abel/libtest/STest.kt
@@ -0,0 +1,15 @@
+package com.abel.libtest
+
+/**
+ * <pre>
+ *     e-mail : leisicheng@baffedu.com
+ *     time   : 2019/08/27
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ *
+ * @author Abel
+ */
+class STest {
+    fun add(i: Int, i1: Int): Int = i + i1
+}
